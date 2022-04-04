e8835cf96ddea8792d93349f3c70b7277a07d8f4
Pedro Sérgio <pedro.oliveira@nave.rs>
2022-04-04 16:45:59 -0300

    amusement-park-improvements

---
+++ amusement-park-improvements/attendee.rb
@@ -0,0 +1,27 @@
+class Attendee
+  def initialize(height)
+    @height = height
+    @pass_id = nil
+  end
+
+  def issue_pass!(pass_id)
+    @pass_id = pass_id
+  end
+
+  def revoke_pass!
+    @pass_id = nil
+  end
+  
+  def has_pass?
+    @pass_id != nil
+  end
+
+  def fits_ride?(ride_minimum_height)
+    @height >= ride_minimum_height
+  end
+
+  def allowed_to_ride?(ride_minimum_height)
+    fits_ride?(ride_minimum_height) and has_pass?
+  end
+
+end
